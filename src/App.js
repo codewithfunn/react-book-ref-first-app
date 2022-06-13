789f6d616cde8e20205be97996f08ebdd99be308
jaspreet09915 <jaspreetsingh09915@gmail.com>
2022-06-13 13:59:36 +0530

    8th js Class nd multiple Component Instance

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import List from './components/List';
 import './App.css';
- 
+// javaScript Classes
+
+// Class Defination
+class Developer{
+  constructor(firstName, lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  getName(){
+    return this.firstName +' ' + this.lastName
+  }
+}
+
+//  Class initialization
+const jaspreet = new Developer('Jaspreet','Singh')
+
+console.log(jaspreet.getName());
+
+// another class Instantiation
+const simran = new Developer("Simran" , "kaur")
+console.log(simran.getName())
 function App() {
  
   return (
+    // a JavaScript class definition exists, one can create multiple instances of it. It is similar to a React component, which has only one component definition, but can have multiple component instances:
     <div className=" my-6 App ">
        <label htmlFor="Search " className=' '>Search: </label>
        <input type="text" id='search' className='hover:border-2 hover:border-red-200 mx-auto'  />
+       {/* Creating an instance of List Component */}
+      <List/>
+      {/* Creating an another instance of List Component */}
       <List/>
     </div>
   );
