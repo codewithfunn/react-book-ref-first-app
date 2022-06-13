6a393b2a6dcf70b0b88d64040172f6ca68ffb986
jaspreet09915 <jaspreetsingh09915@gmail.com>
2022-06-13 13:12:30 +0530

    5th excercise list and map implimentation

--- src/App.js
+++ src/App.js
@@ -1,44 +1,19 @@
 import './App.css';
-// List in React.js [list] / list Object in react [{list_object}] 
-const list =[
-  {
-    title:'React',
-    url :'http://reactjs.org/',
-    author:'Jordan walke',
-    num_comment:3,
-    point :4,
-    objectId:0,
-  },
-  {
-    title:'Redux',
-    url :'http://redux.js.org/',
-    author:'Dan Abramov, Andrew Clark',
-    num_comment:2,
-    point :5,
-    objectId:1,
-  }
-]
-
+// List in React.js [list]  
+const number = [2,4,5,2,15];
+const newNumber = number.map(function(num){
+  return num*2;
+})
+console.log(newNumber);
 function App() {
-  console.log(list);
+ 
   return (
     <div className="App">
     
 
        <label htmlFor="Search">Search: </label>
        <input type="text" id='search'  />
-       {/* Iterate the list_Object using map func */}
-        {list.map((item ,objectId)=>{
-        return<ul>
-          <li>title : {item.title}</li>
-          <li>url : {item.url}</li>
-          <li>author : {item.author}</li>
-          <li>num_of_Comment : {item.num_comment}</li>
-          <li>Point : {item.point}</li>
-          <li>ObjectId : {objectId}</li>
-      </ul>
-        })}
-      
+  
     </div>
   );
 }
