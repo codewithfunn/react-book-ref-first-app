b0512fb8a8ca3a591c69d6f50f22d9e02b7da9ba
jaspreet09915 <jaspreetsingh09915@gmail.com>
2022-06-13 12:52:59 +0530

    4rd exercise list_obj in reactjs

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import './App.css';
-// func in js
-function getTitle(title){
-  return title;
-}
+// List in React.js [list] / list Object in react [{list_object}] 
+const list =[
+  {
+    title:'React',
+    url :'http://reactjs.org/',
+    author:'Jordan walke',
+    num_comment:3,
+    point :4,
+    objectId:0,
+  },
+  {
+    title:'Redux',
+    url :'http://redux.js.org/',
+    author:'Dan Abramov, Andrew Clark',
+    num_comment:2,
+    point :5,
+    objectId:1,
+  }
+]
+
 function App() {
-  
+  console.log(list);
   return (
     <div className="App">
-       <h1>
-        hello {getTitle('React')}      
-       </h1>
+    
 
        <label htmlFor="Search">Search: </label>
        <input type="text" id='search'  />
+       {/* Iterate the list_Object using map func */}
+        {list.map((item ,objectId)=>{
+        return<ul>
+          <li>title : {item.title}</li>
+          <li>url : {item.url}</li>
+          <li>author : {item.author}</li>
+          <li>num_of_Comment : {item.num_comment}</li>
+          <li>Point : {item.point}</li>
+          <li>ObjectId : {objectId}</li>
+      </ul>
+        })}
+      
     </div>
   );
 }
